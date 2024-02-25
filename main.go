d1a39b3ac893f5439bb9ca8c01c236c29d1f9898
seredaes <seredaes@yandex.com>
2024-02-25 19:15:05 +0300

    commit 1

--- main.go
+++ main.go
@@ -2,15 +2,11 @@ package main
 
 import (
 	"seredaes/go-passmem/App/Env"
-	Logger "seredaes/go-passmem/App/Log"
 )
 
 func main() {
 
 	// Load ENV
 	Env.LoadEnv()
-
-	Logger.Log("Test", "danger")
-	Logger.Log("Test", "danger")
 
 }
